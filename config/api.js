4ae9c8a60540094b44b0ff55f2f7d490e8d1d98a
robbychristian <61650887+robbychristian@users.noreply.github.com>
2023-06-08 18:03:12 +0800

    Update api.js

--- config/api.js
+++ config/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const DEV_URL = 'http://10.0.2.2:8000/';
+export const PROD_URL = 'https://hbnaevis.online/';
 
 export const api = axios.create({
-  baseURL: DEV_URL,
+  baseURL: PROD_URL,
 });
 
 api.defaults.timeout = 60000;
